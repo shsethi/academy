28e8a326c7504f27abb69ed54acd74166636a9a6
pranavkeshav <pranavkeshav@gmail.com>
2015-04-06 19:09:32 +0530

    working modify.php

--- modify.php
+++ modify.php
@@ -2,8 +2,88 @@
 
 require 'config.php';
 
+$table = null;
+
+switch ($_POST['name']) {
+	case 'uname':
+		$table = 'registration';
+		$type = PDO::PARAM_STR;
+		break;
+	case 'gender':
+		$table = 'registration';
+		$type = PDO::PARAM_STR;
+		break;
+	case 'dob':
+		$table = 'registration';
+		$type = PDO::PARAM_STR;
+		break;
+	case 'fname':
+		$table = 'registration';
+		$type = PDO::PARAM_STR;
+		break;
+	case 'mname':
+		$table = 'registration';
+		$type = PDO::PARAM_STR;
+		break;
+	case 'email':
+		$table = 'registration';
+		$type = PDO::PARAM_STR;
+		break;
+	case 'phoneno':
+		$table = 'registration';
+		$type = PDO::PARAM_STR;
+		break;
+	case 'address':
+		$table = 'registration';
+		$type = PDO::PARAM_STR;
+		break;
+	case 'center':
+		$table = 'registration';
+		$type = PDO::PARAM_STR;
+		break;
+	case 'sports':
+		$table = 'registration';
+		$type = PDO::PARAM_STR;
+		break;
+	case 'trainingType':
+		$table = 'registration';
+		$type = PDO::PARAM_STR;
+		break;
+	case 'timings':
+		$table = 'registration';
+		$type = PDO::PARAM_STR;
+		break;
+	
+	case 'weight':
+		$table = 'fitness';
+		$type = PDO::PARAM_INT;
+		break;
+	case 'height':
+		$table = 'fitness';
+		$type = PDO::PARAM_INT;
+		break;
+	default:
+		echo "Unexpected error in switch case";
+		break;
+}
+
 try {
-	echo "Hello Git";
+	print_r($_POST);
+
+	# dbh means database handle
+	$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	# error handling method
+	$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+	# sth means statement handle
+	# prepared statement
+	$sth = $dbh->prepare("UPDATE ".$table." SET ". $_POST['name'] ." = :value
+							WHERE userId = :pk");
+	$sth->bindParam(':value',$_POST['value'],$type);
+	$sth->bindParam(':pk',$_POST['pk'],PDO::PARAM_STR);
+	$sth->execute();
+
+	echo "Update successful";
 } catch (Exception $e) {
 	echo $e->getMessage();
 }
