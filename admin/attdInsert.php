ee0b62dd10ea1bd64d24a0306a43ac0a98ef0d5e
pranavkeshav <pranavkeshav@gmail.com>
2015-04-10 20:19:15 +0530

    working attdInsert.php

--- admin/attdInsert.php
+++ admin/attdInsert.php
@@ -1,4 +1,35 @@
 <?php 
 
-print_r($_POST);
+require '../config.php';
+
+try {
+	# dbh means database handle
+	$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	# error handling method
+	$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+	foreach ($_POST as $key => $value) {
+
+		//For all array elements in $_POST other than date
+		if (strcasecmp($key, "date") !== 0) {
+			
+			# sth means statement handle
+			# prepared statement
+			$sth = $dbh->prepare("INSERT into attendance (userId,date,status)
+									VALUES (:userId,:date,:status)");
+
+			$sth->bindParam(':userId',$key,PDO::PARAM_STR); //$key holds userId
+			$sth->bindParam(':date',$_POST['date'],PDO::PARAM_STR);
+			$sth->bindParam(':status',$value,PDO::PARAM_STR); //$value holds attendance status
+
+			$sth->execute();
+		}
+	}
+	
+	echo "Inserted successfully";
+
+	$dbh = null;
+} catch (Exception $e) {
+	echo $e->getMessage();
+}
  ?>
