f2bc207890ae72b7bc6f28155546d1890de863fb
pranavkeshav <pranavkeshav@gmail.com>
2015-04-13 13:25:09 +0530

    attdInsert_Final

--- admin/attdInsert.php
+++ admin/attdInsert.php
@@ -8,18 +8,19 @@ try {
 	# error handling method
 	$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
+	# sth means statement handle
+	# prepared statement
+	$sth = $dbh->prepare("INSERT into attendance (userId,date,status)
+							VALUES (:userId,:date,:status)");
+
+	$sth->bindParam(':date',$_POST['date'],PDO::PARAM_STR);
+
 	foreach ($_POST as $key => $value) {
 
 		//For all array elements in $_POST other than date
 		if (strcasecmp($key, "date") !== 0) {
 			
-			# sth means statement handle
-			# prepared statement
-			$sth = $dbh->prepare("INSERT into attendance (userId,date,status)
-									VALUES (:userId,:date,:status)");
-
 			$sth->bindParam(':userId',$key,PDO::PARAM_STR); //$key holds userId
-			$sth->bindParam(':date',$_POST['date'],PDO::PARAM_STR);
 			$sth->bindParam(':status',$value,PDO::PARAM_STR); //$value holds attendance status
 
 			$sth->execute();
