fca943b993aa82f869eaff0de56b6a8eee4e1596
pranavkeshav <pranavkeshav@gmail.com>
2015-04-19 15:09:29 +0530

    attendance bug removed

--- admin/attendance.php
+++ admin/attendance.php
@@ -32,164 +32,169 @@ header("Location: http://localhost/aceacademy.com/");
 			
 			<div class="row">
 				<div class="col-lg-12">
-	
-						<div class="span5">
-							<form role="form" id="form1" name="form1" method="post" >
+					
+					<div class="span5">
+						<form role="form" id="form1" name="form1" method="post" >
 							
-								<div class="row">
-									
-									<div class="form-group">
-										<label for="date" class="col-lg-2 control-label">Date</label>
-										<div class="col-lg-6">
-											<input type="date" id= "datepicker" class="form-control" name="date" 
-											value="<?php if(isset ($_POST['date'])) echo$_POST['date']; else echo date("Y-m-d"); ?>">
-											<!-- <input type="text" class="form-control" id="datepicker"> -->
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="form-group">
-										<label for="Center" class="col-lg-2 control-label">Center</label>
-										<div class="col-lg-6">
-											<select class="form-control" name= "centerId">
-												<option value="all">All</option>
-												<option value="01">Sarabha Nagar</option>
-												<option value="02">Khalsa College</option>
-												<option value="03">BCM college</option>
-												<option value="04">Govt. College</option>
-												<option value="05">Sports College</option>
-											</select>
-										</div>
-									</div>
-								</div>
+							<div class="row">
 								
-								<div class="row">
-									<div class="form-group">
-										<label for="Sports" class="col-lg-2 control-label">Sports</label>
-										<div class="col-lg-6">
-											<select class="form-control" name= "sportsId" >
-												<option value="all">All</option>
-												<option value="BD">Badminton</option>
-												<option value="CK">Cricket</option>
-												<option value="FB">Football</option>
-												<option value="MA">Martial Arts</option>
-												<option value="TT">Table Tennis</option>
-												<option value="LT">Tennis</option>
-											</select>
-										</div>
+								<div class="form-group">
+									<label for="date" class="col-lg-2 control-label">Date</label>
+									<div class="col-lg-6">
+										<input type="date" id= "datepicker" class="form-control" name="date"
+										value="<?php if(isset ($_POST['date'])) echo$_POST['date']; else echo date("Y-m-d"); ?>">
+										<!-- <input type="text" class="form-control" id="datepicker"> -->
 									</div>
-								</div>					
-
-								<div class="col-md-3">
-									<input class="btn btn-primary" type="submit" value="Go">
 								</div>
-
-							</form>
-						</div>
-						<hr>
-						<!-- Attendance Table -->
-										
-						<div class="alert" style="display:none">
+							</div>
+							<div class="row">
+								<div class="form-group">
+									<label for="Center" class="col-lg-2 control-label">Center</label>
+									<div class="col-lg-6">
+										<select class="form-control" name= "centerId">
+											<option value="all">All</option>
+											<option value="01">Sarabha Nagar</option>
+											<option value="02">Khalsa College</option>
+											<option value="03">BCM college</option>
+											<option value="04">Govt. College</option>
+											<option value="05">Sports College</option>
+										</select>
+									</div>
+								</div>
+							</div>
 							
-							<!-- <strong>Success!</strong> Attendance marked -->
-						</div>
-
-						<ul class="nav nav-tabs" id="myTab">
-							<li class="active"><a data-toggle="tab" href="#viewattd">View Attendance</a></li>
-							<li><a data-toggle="tab" href="#markattd">Mark Attendance</a></li>
-						</ul>
-						<div class="tab-content">
+							<div class="row">
+								<div class="form-group">
+									<label for="Sports" class="col-lg-2 control-label">Sports</label>
+									<div class="col-lg-6">
+										<select class="form-control" name= "sportsId" >
+											<option value="all">All</option>
+											<option value="BD">Badminton</option>
+											<option value="CK">Cricket</option>
+											<option value="FB">Football</option>
+											<option value="MA">Martial Arts</option>
+											<option value="TT">Table Tennis</option>
+											<option value="LT">Tennis</option>
+										</select>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<input class="btn btn-primary" type="submit" value="Go">
+							</div>
+						</form>
+					</div>
+					<hr>
+					<!-- Attendance Table -->
+					
+					<div class="alert" style="display:none">
+						
+						<!-- <strong>Success!</strong> Attendance marked -->
+					</div>
+					<ul class="nav nav-tabs" id="myTab">
+						<li class="active"><a data-toggle="tab" href="#viewattd">View Attendance</a></li>
+						<li><a data-toggle="tab" href="#markattd">Mark Attendance</a></li>
+					</ul>
+					<div class="tab-content">
 						<div class="tab-pane active" id="viewattd">
 							<div class="span7 offset2">
-									
-							<div class="table-responsive">
-								<table id="viewAttdTable" class="table table-striped table-bordered dataTable no-footer" border="1">
-									
-									<thead>
-										<tr>
-											<th>S.NO</th>
-											<th>Student Name</th>
-											<th>UID</th>
-											<th>Absent(A) Present(P) </th>
-										</tr>
-									</thead>
-									<tbody>
-										<?php
-												require '../config.php';
-										try {
-												# dbh means database handle
-												$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-												# error handling method
-												$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-												$query = "SELECT registration.userId,uname,status FROM registration
-															LEFT JOIN (SELECT * FROM attendance
-																WHERE date like :date) as attd
-																ON registration.userId = attd.userId";
-												$sth = $dbh->prepare($query);
-												$sth->bindParam(':date',$_POST['date'],PDO::PARAM_STR);
-
-												//If all was selected then where conditions would not be added
-												if (strcasecmp("all", $_POST['sportsId']) !== 0) {
-												//Concatenate
-													$query = $query . " JOIN stud_sports
-																		ON registration.userId = stud_sports.userId 
-																		WHERE sportsId = :sportsId";
-													$sth = $dbh->prepare($query);
-													
-													
-													if (strcasecmp("all", $_POST['centerId']) !== 0) {
-														$query = $query . " AND centerId = :centerId";
-														$sth = $dbh->prepare($query);
-														$sth->bindParam(':centerId',$_POST['centerId'],PDO::PARAM_STR);
-													}
-
-													$sth->bindParam(':sportsId',$_POST['sportsId'],PDO::PARAM_STR);
-												}
-												else{
-													if (strcasecmp("all", $_POST['centerId']) !== 0) {
-														$query = $query . " WHERE centerId = :centerId";
-														$sth = $dbh->prepare($query);
-														$sth->bindParam(':centerId',$_POST['centerId'],PDO::PARAM_STR);
-													}
-												}
-												$sth->execute();
-												$count=0;
-												while($obj = $sth->fetch()) {
-												$count++;
-												// print_r($obj);
-												?>
-										<tr>
-											<td> <?php echo $count; ?> </td>
-											<td> <?php echo $obj['uname'] ; ?> 	</td>
-											<td> <?php echo $obj['userId'] ; ?> 	</td>
-											<td>
-												<label >
-													 <?php 
-													 if( $obj['status']=== NULL)
-													 	echo "Not marked" ;
-													 else 
-													 	echo $obj['status'] ;?>
-												</label>
-											</td>
-										</tr>
-										<?php
-										}
+								
+								<div class="table-responsive">
+									<table id="viewAttdTable" class="table table-striped table-bordered dataTable no-footer" border="1">
 										
-										}
-										catch (Exception $e) {
-										echo $e->getMessage();
-										}
-										?>
-									</tbody>
-								</table>
-							</div>
-
+										<thead>
+											<tr>
+												<th>S.NO</th>
+												<th>Student Name</th>
+												<th>UID</th>
+												<th>Absent(A) Present(P) </th>
+											</tr>
+										</thead>
+										<tbody>
+											<?php
+											require '../config.php';
+											try {
+													# dbh means database handle
+													$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+													# error handling method
+													$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+													$query = "SELECT registration.userId,uname,status FROM registration
+																LEFT JOIN (SELECT * FROM attendance
+																	WHERE date like :date) as attd
+																	ON registration.userId = attd.userId";
+													
+													$sth = $dbh->prepare($query);
+													$sth->bindParam(':date',$_POST['date'],PDO::PARAM_STR);
+													$sth->execute();
+													//If all was selected then where conditions would not be added
+													if (strcasecmp("all", $_POST['sportsId']) !== 0) {
+													//Concatenate
+														$query = $query . " JOIN stud_sports
+																			ON registration.userId = stud_sports.userId
+																			WHERE sportsId = :sportsId";
+														
+														
+														if (strcasecmp("all", $_POST['centerId']) !== 0) {
+															$query = $query . " AND centerId = :centerId";
+															$sth = $dbh->prepare($query);
+															$sth->bindParam(':centerId',$_POST['centerId'],PDO::PARAM_STR);
+															$sth->bindParam(':sportsId',$_POST['sportsId'],PDO::PARAM_STR);
+															$sth->bindParam(':date',$_POST['date'],PDO::PARAM_STR);
+															$sth->execute();
+														}
+														else{
+															$sth = $dbh->prepare($query);
+															$sth->bindParam(':sportsId',$_POST['sportsId'],PDO::PARAM_STR);
+															$sth->bindParam(':date',$_POST['date'],PDO::PARAM_STR);
+																$sth->execute();
+														}
+														
+													}
+													else{
+														if (strcasecmp("all", $_POST['centerId']) !== 0) {
+															$query = $query . " WHERE centerId = :centerId";
+															$sth = $dbh->prepare($query);
+															$sth->bindParam(':centerId',$_POST['centerId'],PDO::PARAM_STR);
+															$sth->bindParam(':date',$_POST['date'],PDO::PARAM_STR);
+															$sth->execute();
+														}
+													}
+													$count=0;
+													while($obj = $sth->fetch()) {
+													$count++;
+													// print_r($obj);
+											?>
+											<tr>
+												<td> <?php echo $count; ?> </td>
+												<td> <?php echo $obj['uname'] ; ?> 	</td>
+												<td> <?php echo $obj['userId'] ; ?> 	</td>
+												<td>
+													<label >
+														<?php
+														if( $obj['status']=== NULL)
+															echo "Not marked" ;
+														else
+														echo $obj['status'] ;?>
+													</label>
+												</td>
+											</tr>
+											<?php
+											}
+											
+											}
+											catch (Exception $e) {
+											echo $e->getMessage();
+											}
+											?>
+										</tbody>
+									</table>
+								</div>
 							</div>
 						</div>
 						<div class="tab-pane" id="markattd">
 							<form role="form" id="form2" name="form2" method="post" action="attdInsert.php">
 								
-							 <!-- <input type="date" id= "datepicker" class="form-control" name="date" > -->
+								<!-- <input type="date" id= "datepicker" class="form-control" name="date" > -->
 								<div class="span7 offset2">
 									<div class="form-group">
 										<div class="table-responsive">
@@ -206,58 +211,67 @@ header("Location: http://localhost/aceacademy.com/");
 												<tbody>
 													<?php
 															require '../config.php';
-											try{
-												# dbh means database handle
-												$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-												# error handling method
-												$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-												$query = "SELECT registration.userId,uname,status FROM registration
+													try {
+													# dbh means database handle
+													$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+													# error handling method
+													$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+													$query = "SELECT registration.userId,uname,status FROM registration
 															LEFT JOIN (SELECT * FROM attendance
 																WHERE date like :date) as attd
 																ON registration.userId = attd.userId";
-												$sth = $dbh->prepare($query);
-												$sth->bindParam(':date',$_POST['date'],PDO::PARAM_STR);
-
-												//If all was selected then where conditions would not be added
-												if (strcasecmp("all", $_POST['sportsId']) !== 0) {
-												//Concatenate
-													$query = $query . " JOIN stud_sports
-																		ON registration.userId = stud_sports.userId 
-																		WHERE sportsId = :sportsId";
+													
 													$sth = $dbh->prepare($query);
+													$sth->bindParam(':date',$_POST['date'],PDO::PARAM_STR);
+													$sth->execute();
+													//If all was selected then where conditions would not be added
+													if (strcasecmp("all", $_POST['sportsId']) !== 0) {
+													//Concatenate
+													$query = $query . " JOIN stud_sports
+																		ON registration.userId = stud_sports.userId
+																		WHERE sportsId = :sportsId";
 													
 													
 													if (strcasecmp("all", $_POST['centerId']) !== 0) {
 														$query = $query . " AND centerId = :centerId";
 														$sth = $dbh->prepare($query);
 														$sth->bindParam(':centerId',$_POST['centerId'],PDO::PARAM_STR);
+														$sth->bindParam(':sportsId',$_POST['sportsId'],PDO::PARAM_STR);
+														$sth->bindParam(':date',$_POST['date'],PDO::PARAM_STR);
+														$sth->execute();
 													}
-
-													$sth->bindParam(':sportsId',$_POST['sportsId'],PDO::PARAM_STR);
-												}
-												else{
+													else{
+														$sth = $dbh->prepare($query);
+														$sth->bindParam(':sportsId',$_POST['sportsId'],PDO::PARAM_STR);
+														$sth->bindParam(':date',$_POST['date'],PDO::PARAM_STR);
+															$sth->execute();
+													}
+													
+													}
+													else{
 													if (strcasecmp("all", $_POST['centerId']) !== 0) {
 														$query = $query . " WHERE centerId = :centerId";
 														$sth = $dbh->prepare($query);
 														$sth->bindParam(':centerId',$_POST['centerId'],PDO::PARAM_STR);
+														$sth->bindParam(':date',$_POST['date'],PDO::PARAM_STR);
+														$sth->execute();
 													}
-												}
-												$sth->execute();
-												$count=0;
-												while($obj = $sth->fetch()) {
-												$count++;
-												// print_r($obj);
-															?>
+													}
+													$count=0;
+													while($obj = $sth->fetch()) {
+													$count++;
+													// print_r($obj);
+													?>
 													<tr>
 														<td> <?php echo $count; ?> </td>
 														<td> <?php echo $obj['uname'] ; ?> 	</td>
 														<td> <?php echo $obj['userId'] ; ?> 	</td>
 														<td>
 															<label class="radio-inline">
-																<input type="radio" name="<?php echo $obj['userId'];?>" <?php if (strcasecmp ($obj['status'],"P")) {echo  "checked";} ?> value="P"  > P
+																<input type="radio" name="<?php echo $obj['userId'];?>" <?php if (strcasecmp ($obj['status'],"P") == 0) {echo  "checked";} ?> value="P"  > P
 															</label>
 															<label class="radio-inline">
-																<input type="radio" name="<?php echo $obj['userId'];?>" <?php if (strcasecmp ($obj['status'],"A")) {echo  "checked";} ?> value="A"  > A
+																<input type="radio" name="<?php echo $obj['userId'];?>" <?php if (strcasecmp ($obj['status'],"A") == 0) {echo  "checked";} ?> value="A"  > A
 															</label>
 														</td>
 													</tr>
@@ -280,11 +294,11 @@ header("Location: http://localhost/aceacademy.com/");
 							</form>
 						</div>
 						
-						</div>
+					</div>
 				</div>
-						
-				</div>
+				
 			</div>
+		</div>
 	</div>
 	
 	<script src="../js/jquery.min.js"></script>
@@ -295,26 +309,23 @@ header("Location: http://localhost/aceacademy.com/");
 		$("#uidtable").DataTable(); //initializing Datatable plugin
 		$("#viewAttdTable").DataTable(); //initializing Datatable plugin
 	});
-
 	function today(){
-    	var d = new Date();
-	    var curr_date = d.getDate();
-	    var curr_month = d.getMonth() + 1;
+	var d = new Date();
+	var curr_date = d.getDate();
+	var curr_month = d.getMonth() + 1;
 		
 		if(curr_month<10)
-		curr_month= '0'+curr_month; 
+		curr_month= '0'+curr_month;
 		
 		if(curr_date<10)
-		curr_date= '0'+curr_date; 
-	   
-	    var curr_year = d.getFullYear();
-	    return(curr_year+ "-" + curr_month + "-" + curr_date  );
+		curr_date= '0'+curr_date;
+	
+	var curr_year = d.getFullYear();
+	return(curr_year+ "-" + curr_month + "-" + curr_date  );
 	}
 	
 	// $("#datepicker").val(today());
-
 	</script>
 	<script src="js/markattendance.js"></script>
-
 </body>
 <?php include('/includes/footer.php'); ?>
